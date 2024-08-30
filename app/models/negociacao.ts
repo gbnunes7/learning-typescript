8a64eb1239f52268100084ebe993f11a8d4c19e9
Gabriel Melo <bielrj170@gmail.com>
2024-08-30 19:03:08 -0300

    aprendendo os bruacos que o readonly deixa para objetos complexos

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -1,11 +1,14 @@
 export class Negociacao {
 	constructor(
-		public readonly data:Date, 
+		private _data:Date, 
 		public readonly quantidade:number, 
 		public readonly valor:number
 	) {}
 
-	get volume() {
+	get data():Date {
+		return this._data
+	}
+	get volume():number {
 		return this.valor * this.quantidade;
 	}
 }
